e0d966a21a8cf99e6e552d6950c518f1ab3ec8ca
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2022-07-30 13:30:47 -0300

    aprendendo fundamentos de interface

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,24 +1,26 @@
-// gerente é um funcionario, gerente herda da class funcionario
-public class Gerente extends Funcionario {
+// gerente é um Funcionario, gerente herda da class Funcionario, assina o contrato autenticavel, é um autenticavel
+public class Gerente extends Funcionario implements Autenticavel{
 	
 	private int senha;
-	
+
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificação do Gerente");
+		return super.getSalario();
+	}
+
+	@Override
 	public void setSenha(int senha) {
 		this.senha = senha;
+		
 	}
-	
+
+	@Override
 	public boolean autentica(int senha) {
 		if(this.senha == senha) {
 			return true;
 		} else {
 			return false;
 		}
-	}
-	
-	
-	public double getBonificacao() {
-		System.out.println("Chamando o método de bonificação do Gerente");
-		return super.getSalario();
 	}
 	
 
