70287290765d070e4a8c9f93270e9a82c4d26fcd
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2022-07-31 06:48:03 -0300

    implementando tributaveis, revisando conceito de heranca, exemplos de heranca

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,7 +1,11 @@
 // gerente é um Funcionario, gerente herda da class Funcionario, assina o contrato autenticavel, é um autenticavel
 public class Gerente extends Funcionario implements Autenticavel{
 	
-	private int senha;
+	private AutenticacaoUtil autenticador;
+
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
 
 	public double getBonificacao() {
 		System.out.println("Chamando o método de bonificação do Gerente");
@@ -10,18 +14,16 @@ public class Gerente extends Funcionario implements Autenticavel{
 
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 		
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
+		return this.autenticador.autentica(senha);
 	}
+
+	
 	
 
 }
